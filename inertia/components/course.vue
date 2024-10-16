f96707d4861ae84c4d6f826fe6af4df66f4eb087
kooliarvuti <opetaja@tpt.lan>
2024-10-16 13:02:36 +0300

    back to where it all started
    a

--- inertia/components/course.vue
+++ inertia/components/course.vue
@@ -1,6 +1,7 @@
 <script lang="ts">
 import { ref } from 'vue'
-import { RadioGroup, RadioGroupLabel, RadioGroupOption } from '@headlessui/vue'
+import { RadioGroup, RadioGroupLabel, RadioGroupOption, DisclosureButton, Disclosure, DisclosurePanel } from '@headlessui/vue'
+import { ChevronUpIcon } from '@heroicons/vue/20/solid'
 
 const ratingOptions = [
   { name: '1' },
@@ -15,6 +16,10 @@ export default {
     RadioGroup,
     RadioGroupLabel,
     RadioGroupOption,
+    Disclosure,
+    DisclosurePanel,
+    DisclosureButton,
+    ChevronUpIcon
   },
   props: {
     name: String
@@ -33,13 +38,13 @@ export default {
 <template>
   <div>
     <div class="flex items-center justify-between">
-      <h2 class="text-lg font-medium text-gray-900">{{ name }}</h2>
+      <h2 class="text-lg font-semibold text-gray-900">{{ name }}</h2>
     </div>
 
-    <RadioGroup v-model="mem" class="mt-2">
+    <RadioGroup v-model="mem" class="mt-2" name="rating">
       <RadioGroupLabel class="sr-only"> Vali hinnang </RadioGroupLabel>
-      <div class="grid grid-cols-3 gap-3 sm:grid-cols-6">
-        <RadioGroupOption as="template" v-for="option in ratingOptions" :key="option.name" :value="option"
+      <div class="grid grid-cols-5 gap-3 sm:grid-cols-5">
+        <RadioGroupOption as="template" v-for="option in ratingOptions" :key="option.name" :value="option.name"
           v-slot="{ checked }">
           <div
             :class="['cursor-pointer focus:outline-none', checked ? 'bg-indigo-600 border-transparent text-white hover:bg-indigo-700' : 'bg-white border-gray-200 text-gray-900 hover:bg-gray-50', 'border rounded-md py-3 px-3 flex items-center justify-center text-sm font-medium uppercase sm:flex-1']">
@@ -48,15 +53,31 @@ export default {
             </RadioGroupLabel>
           </div>
         </RadioGroupOption>
-        <div>
-          <label for="comment" class="block text-sm font-medium text-gray-700">Add your comment</label>
-          <div class="mt-1">
-            <textarea rows="4" name="comment" id="comment"
-              class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
-          </div>
-        </div>
+
+
       </div>
 
     </RadioGroup>
+
+    <div class="w-full px-4 ">
+      <div class="mx-auto w-full max-w-md rounded-2xl bg-white p-2">
+        <Disclosure v-slot="{ open }">
+          <DisclosureButton
+            class="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500/75">
+            Kommentaar
+            <ChevronUpIcon :class="open ? 'rotate-180 transform' : ''" class="h-5 w-5 text-purple-500" />
+          </DisclosureButton>
+          <DisclosurePanel class="px-4 pb-2 pt-4 text-sm text-gray-500">
+            <div>
+              <label for="comment" class="block text-sm font-medium text-gray-700">Add your comment</label>
+              <div class="mt-1">
+                <textarea rows="4" name="comment" id="comment"
+                  class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+              </div>
+            </div>
+          </DisclosurePanel>
+        </Disclosure>
+      </div>
+    </div>
   </div>
 </template>
--- inertia/pages/feedback.vue
+++ inertia/pages/feedback.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   import { Head } from '@inertiajs/vue3'
-  import Course from '#components/course'
+  import Course from '../components/course.vue'
   const courses = [
     "Matemaatika",
     "Inglise keel",
@@ -10,8 +10,26 @@
 </script>
 
 <template>
-    <Head title="Feedback" />
-    <div class="container mx-auto px-4" v-for="course in courses">
-        <Course :name="course"/>
+
+  <Head title="Feedback" />
+
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
+    <div class="max-w-3xl mx-auto my-20">
+      <form action="#" method="post">
+        <div class="bg-white shadow overflow-hidden rounded-md">
+          <ul role="list" class="divide-y divide-gray-200">
+            <li v-for="course in courses" class="px-6 py-4" :key="course">
+              <Course :name="course" />
+            </li>
+          </ul>
+        </div>
+        <button type="submit"
+          class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <EnvelopeIcon class="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
+          Saada ära!
+        </button>
+      </form>
     </div>
+  </div>
 </template>
