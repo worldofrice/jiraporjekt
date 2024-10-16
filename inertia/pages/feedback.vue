98610c5c96c120a8d7601ab361f521481c3e75e8
kooliarvuti <opetaja@tpt.lan>
2024-10-16 13:13:07 +0300

    Update feedback.vue

--- inertia/pages/feedback.vue
+++ inertia/pages/feedback.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
   import { Head } from '@inertiajs/vue3'
   import Course from '../components/course.vue'
+import { EnvelopeIcon } from '@heroicons/vue/20/solid'
   const courses = [
     "Matemaatika",
     "Inglise keel",
@@ -16,7 +17,7 @@
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <!-- We've used 3xl here, but feel free to try other max-widths based on your needs -->
     <div class="max-w-3xl mx-auto my-20">
-      <form action="#" method="post">
+      <form action="#" method="post" class="justify-center flex flex-col">
         <div class="bg-white shadow overflow-hidden rounded-md">
           <ul role="list" class="divide-y divide-gray-200">
             <li v-for="course in courses" class="px-6 py-4" :key="course">
@@ -25,7 +26,7 @@
           </ul>
         </div>
         <button type="submit"
-          class="inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          class="inline-flex justify-center items-center self-center px-6 py-2 my-5 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
           <EnvelopeIcon class="-ml-1 mr-3 h-5 w-5" aria-hidden="true" />
           Saada ära!
         </button>
